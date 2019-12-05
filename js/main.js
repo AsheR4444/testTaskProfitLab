d131ee2cb6ba90ac99f3607793a0baa1a79b1d94
Илья Тюрин <il08082000@gmail.com>
2019-12-06 00:09:53 +0700

    Сломал кнопку, надо починить(

--- js/main.js
+++ js/main.js
@@ -23,10 +23,5 @@ function init() {
 // == /yandex map ==
 
 // == header-button ==
-const menuBtn = document.getElementById('menuBtn'); // получаем кнопку
 
-menuBtn.addEventListener('click', function(event) {
-  event.preventDefault(); // убираю дефолтное действие
-  menuBtn.classList.toggle('header-button_active'); // делаю смену класса по клику
-});
 // == /header-button ==

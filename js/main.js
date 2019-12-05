3d8ff990154d97d8340d68695e71ea0736e42b77
Илья Тюрин <il08082000@gmail.com>
2019-12-05 21:34:21 +0700

    Добавлена карта и баллун

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// == yandex map ==
+ymaps.ready(init);
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [55.991923665400876, 92.98853461287474],
+    zoom: 16,
+    controls: ['zoomControl'],
+    behaviors: []
+  });
+  let placemark = new ymaps.Placemark( // создал placemark указав центр
+    [55.9922510687489, 92.98318649999999],
+    {
+      hintContent: 'Нажми на меня!',
+      balloonContentHeader: '<div class="map-box__section">Офис:</div>',
+      balloonContentBody: '<div class="map-box__adress">ул. Апрельская, 4</div>'
+    },
+    {
+      balloonPanelMaxMapArea: 0 // разрешил balloon выходить за карту
+    }
+  );
+  map.geoObjects.add(placemark); // добавил placemark на карту
+}
+// == /yandex map ==
+
 // == header-button ==
 const menuBtn = document.getElementById('menuBtn'); // получаем кнопку
 
